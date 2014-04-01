e90151c980bc8f4292275830ddb1dd98ab433f2a
Florian Rampp <Florian.Rampp@gmail.com>
2014-04-01 11:16:08 +0200

    added copying of prod to staging DB to Heroku script

--- heroku-config.sh
+++ heroku-config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-#heroku config:add JAVA_OPTS='-Xmx256m -Xss512k -XX:+UseCompressedOops -javaagent:newrelic/newrelic.jar'
+heroku config:add -a chalk-up-api JAVA_OPTS='-Xmx256m -Xss512k -XX:+UseCompressedOops -javaagent:newrelic/newrelic.jar'
+heroku config:add -a chalk-up-api-staging JAVA_OPTS='-Xmx256m -Xss512k -XX:+UseCompressedOops -javaagent:newrelic/newrelic.jar -Dnewrelic.environment=staging'
 
-heroku pgbackups:transfer --app chalk-up-api HEROKU_POSTGRESQL_JADE_URL chalk-up-api-staging::HEROKU_POSTGRESQL_CRIMSON_URL
+# copy content of production DB to staging DB
+#heroku pgbackups:transfer --app chalk-up-api HEROKU_POSTGRESQL_JADE_URL chalk-up-api-staging::HEROKU_POSTGRESQL_CRIMSON_URL
