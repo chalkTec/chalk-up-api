7406d577bd255f09af584d2bd8a7285170e48504
Florian Rampp <Florian.Rampp@gmail.com>
2014-04-05 12:08:30 +0200

    check for valid UIAA grade

--- grails-app/services/chalkup/rest/RouteService.groovy
+++ grails-app/services/chalkup/rest/RouteService.groovy
@@ -2,12 +2,10 @@ package chalkup.rest
 
 import chalkup.exceptions.InvalidModificationException
 import chalkup.exceptions.NotFoundException
-import chalkup.gym.Gym
-import chalkup.gym.Route
-import chalkup.gym.RouteColor
-import chalkup.gym.SportGrade
-import chalkup.gym.SportRoute
+import chalkup.gym.*
 import grails.transaction.Transactional
+import grails.validation.ValidationErrors
+import grails.validation.ValidationException
 import org.springframework.dao.OptimisticLockingFailureException
 
 @Transactional
@@ -25,20 +23,18 @@ class RouteService {
         }
 
         def parent = params['parentPluralizedResourceName']
-        if(parent == null) {
+        if (parent == null) {
             // allow access without parent
-        }
-        else if(parent == 'gyms') {
+        } else if (parent == 'gyms') {
             long gymId = Long.valueOf(params['parentId'])
 
-            if(!Gym.exists(gymId))
+            if (!Gym.exists(gymId))
                 throw new NotFoundException(objectName: 'gym', objectId: gymId)
 
             criteria = criteria.where {
                 gym.id == gymId
             }
-        }
-        else {
+        } else {
             throw new NotFoundException(objectName: parent, objectId: params['parentId'])
         }
         return criteria
@@ -71,33 +67,44 @@ class RouteService {
     void bindSportRoute(SportRoute route, def map) {
         route.properties = map  // name, number, description, foreignId, 4x date
 
-        if(map['type'] && map['type'] != SportRoute.DISCRIMINATOR)
+        if (map['type'] && map['type'] != SportRoute.DISCRIMINATOR)
             throw new InvalidModificationException(reason: "cannot change the type of a route")
 
-        if(map['color'])
+        if (map['color'])
             route.color = RouteColor.valueOf(map['color']['name'])
 
-        if(map['initialGrade'])
-            route.initialGrade = SportGrade.fromUiaaScale(map['initialGrade']['uiaa'])
+        if (map['initialGrade']) {
+            String uiaa = map['initialGrade']['uiaa']
+            if (SportGrade.isUiaaScaleGrade(uiaa)) {
+                route.initialGrade = SportGrade.fromUiaaScale(uiaa)
+            } else {
+                route.errors.reject('chalkup.invalid.grade.message',
+                        [uiaa, 'UIAA'] as Object[], "$uiaa is not a valid UIAA grade");
+            }
+        }
     }
 
     Route update(def id, def map, def params) {
         Route route = findRoute(id)
 
         // TODO: why does this not work automatically?
-        if(map['version'] != null && map['version'] != route.version) {
+        if (map['version'] != null && map['version'] != route.version) {
             throw new OptimisticLockingFailureException("route has changed in the mean time")
         }
 
         // check for changing gym
-        if(map['gym'] && map['gym'].id != route.gym.id)
+        if (map['gym'] && map['gym'].id != route.gym.id)
             throw new InvalidModificationException(reason: "cannot move routes between gyms")
 
         // bind parameters
-        if(route instanceof SportRoute)
+        if (route instanceof SportRoute)
             bindSportRoute(route, map)
         else
             throw new UnsupportedOperationException('cannot bind boulders yet')
+
+        if(route.hasErrors()) {
+            throw new ValidationException("route not valid", route.errors)
+        }
 
         route.save(flush: true)
 
@@ -110,7 +117,7 @@ class RouteService {
     void delete(def id, def map, def params) {
         Route route = findRoute(id)
 
-        if(map['version'] != null && map['version'] != route.version) {
+        if (map['version'] != null && map['version'] != route.version) {
             throw new OptimisticLockingFailureException("route has changed in the mean time")
         }
 
